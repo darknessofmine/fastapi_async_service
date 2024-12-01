1d827f13fe37cba2fb9df589f1b7bc1b7dd41251
Pavel Pischalnikov <doessmth@gmail.com>
2024-12-02 00:08:55 +0500

    "Post" schemas for CRUD

---
+++ api/posts/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, ConfigDict
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from core.models import User
+
+
+class PostBase(BaseModel):
+    title: str
+    text: str
+
+
+class Post(PostBase):
+    id: int
+    user: "User"
+
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
+
+
+class PostCreate(PostBase):
+    ...
+
+
+class PostUpdate(PostCreate):
+    ...
+
+
+class PostUpdatePartial(PostCreate):
+    title: str | None = None
+    text: str | None = None
