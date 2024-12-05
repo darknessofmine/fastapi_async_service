d08c30a1833ecc68c6e4e3a78695b6fcad5f329c
Pavel Pischalnikov <doessmth@gmail.com>
2024-12-05 19:10:02 +0500

    added password field to user model

--- core/models/user.py
+++ core/models/user.py
@@ -15,6 +15,7 @@ class User(Base):
     __tablename__ = "users"
 
     username: Mapped[str] = mapped_column(String(32), unique=True)
+    password: Mapped[str] = mapped_column(String(50))
 
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
     sub_services: Mapped[list["Subscription"] | None] = relationship(
